4a52b35c65680b698e281c3a3ff8de2fc331e6c4
Augustine <smaji08@gmailcom>
2019-12-07 16:49:12 -0500

    basic validation for inputs done

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+function checkFunction(){
+    var checkForm = document.getElementById("checkbox-form");
+    var count = 0;
+    var pwdLength = document.getElementById("pwdLength").value;
+    
+        intPwdLength = parseInt(pwdLength);
+    
+    for (var i = 0; i < checkForm.elements.length; i++ ) {
+        if (checkForm.elements[i].checked == false) {
+            count++;
+        }
+    }
+   
+    if (count == 4){
+        alert("You should select AT LEAST 1 checkbox");
+    }
+
+    if (pwdLength == "" || intPwdLength<8 || intPwdLength >128){
+        alert("Please enter the length between 8 and 128")
+    }
+
+}
+
+
+    
